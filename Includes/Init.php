e222f05cd2d62c0fe3eea25b5454696d6eb6277b
Guillermoscript <arepayquezo@gmail.com>
2022-07-27 20:52:29 -0400

    init for custom classes

---
+++ Includes/Init.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Includes;
+
+use Front\Enqueue as FrontEnqueue;
+
+final class Init
+{
+    
+    public static function get_services()
+    {
+        return [
+            FrontEnqueue::class,
+        ];
+    }
+
+
+    /**
+     * inicializa la clase y retorna una nueva instancia de ela clase
+     */
+
+    private static function instantiate( $class ) 
+    {   
+        $service = new $class();
+        return $service;
+    }
+
+    public static function register_services() 
+    {
+        foreach (self::get_services() as $class ) {
+            $services = self::instantiate( $class );
+            if ( method_exists( $services, 'register' ) ) {
+                $services->register();
+            }
+        }
+    }
+}
